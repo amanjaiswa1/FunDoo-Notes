531db25d687f7ad55b11036e90cc3f16aa9e27d3
amanjaiswa1 <amanjaiswal0608@gmail.com>
2022-11-15 15:15:02 +0530

    Refactor: Encrypting Password While UserRegistration Using Bcrypt

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,7 +1,12 @@
 import User from '../models/user.model';
+import bcrypt from 'bcrypt';
 
 //create a new user registration
 export const registration = async (body) => {
+  const saltRounds = 10;
+  const salt = bcrypt.genSaltSync(saltRounds);
+  const hash = bcrypt.hashSync(body.Password, salt);
+  body.Password = hash;
   const data = await User.create(body);
   return data;
 };
