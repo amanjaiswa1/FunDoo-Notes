9e30f357da158e4ecc566ac307e19d639d021413
amanjaiswa1 <amanjaiswal0608@gmail.com>
2022-11-30 23:19:24 +0530

    [Aman] Modified: RabbitMQ To Send Registered Successefully Mail Using Gmail API

--- src/utils/rabbitmq.js
+++ src/utils/rabbitmq.js
@@ -1,6 +1,8 @@
+import { sendRabbitMail } from './user.util';
+
 var amqp = require('amqplib/callback_api');
 
- export const sender = (registeredData) => {
+export const sender = (registeredData) => {
     amqp.connect('amqp://localhost', function (error0, connection) {
         if (error0) {
             throw error0;
@@ -23,7 +25,7 @@ var amqp = require('amqplib/callback_api');
     });
 }
 
- const receiver = () => {
+const receiver = () => {
     amqp.connect('amqp://localhost', function (error0, connection) {
         if (error0) {
             throw error0;
@@ -43,9 +45,13 @@ var amqp = require('amqplib/callback_api');
 
             channel.consume(queue, function (msg) {
                 console.log(" [x] Received %s", msg.content.toString());
+                let userData = JSON.parse(msg.content.toString());
+                sendRabbitMail(userData.FirstName, userData.LastName, userData.Email);
             }, {
                 noAck: true
             });
+
+
         });
     });
 }
--- src/utils/user.util.js
+++ src/utils/user.util.js
@@ -3,7 +3,7 @@ const { google } = require('googleapis');
 const CLIENT_ID = '742279253300-oe685i163e9p8clhmp3dl0rnbe0udo0t.apps.googleusercontent.com';
 const CLEINT_SECRET = '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';
 const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
-const REFRESH_TOKEN = '1//04n20XiCOwpxwCgYIARAAGAQSNwF-L9Ircm-Rw7UFQbGgUlha7fPfR5HzMWnE9I5VontGWXFojCMZ5w-bMmx7rhlMY84g_SX6-V8';
+const REFRESH_TOKEN = '1//04O8KJ60eyLEUCgYIARAAGAQSNwF-L9IrC7yX5FBeOVXL80bbYEjzFCHoXAz5tZoQq-_lmlcjFQGDlD4ZWuM9dD79QhZyPWuHYhQ';
 
 const oAuth2Client = new google.auth.OAuth2(
     CLIENT_ID,
@@ -34,6 +34,37 @@ export const sendMail = async (Email) => {
             subject: 'Reset Password',
             text: 'Hello from gmail email using API',
             html: '<h1>To Reset Your Password <a href="http://localhost:3000/api/v1/users/resetpassword"> Click Here </a></h1>',
+        };
+
+        const result = await transport.sendMail(mailOptions);
+        return result;
+    } catch (error) {
+        return error;
+    }
+}
+
+export const sendRabbitMail = async (FirstName,LastName,Email) => {
+    try {
+        const accessToken = await oAuth2Client.getAccessToken();
+
+        const transport = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                type: 'OAuth2',
+                user: 'amanjaiswal0608@gmail.com',
+                clientId: CLIENT_ID,
+                clientSecret: CLEINT_SECRET,
+                refreshToken: REFRESH_TOKEN,
+                accessToken: accessToken,
+            },
+        });
+
+        const mailOptions = {
+            from: 'AmanJaiswal <amanjaiswal0608@gmail.com>',
+            to: Email,
+            subject: 'Registered Successfully',
+            text: `Name: ${FirstName} ${LastName} \n \nCongratulations!!! Your account has been created successfully.`,
+           
         };
 
         const result = await transport.sendMail(mailOptions);
